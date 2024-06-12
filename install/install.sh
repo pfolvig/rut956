02a33c19a18fac23a1d0a9c6d3c4c2f885190367
Park <pfolvig@gmail.com>
2024-06-12 14:07:06 +0800

    Made changes to work with the latest firmware 07.07.1 Changed to use TLS1.2 in the mosquitto config Updated the install script to allow for more time to reboot Cleared and templated values for npe.uci Notes.txt to be used for comments on the current build

--- install/install.sh
+++ install/install.sh
@@ -1,13 +1,13 @@
 #!/bin/ash
-opkg update
-opkg install mosquitto-ssl mosquitto-client-ssl
-opkg install python3 python3-pip
-opkg install coreutils-nohup
-opkg install python3-light
-opkg install python3-codecs
-opkg install python3-decimal
-opkg install python3-logging
-opkg install python3-pyserial
+opkg update --force_feeds /etc/opkg/openwrt/distfeeds.conf
+opkg install mosquitto-ssl mosquitto-client-ssl --force_feeds /etc/opkg/openwrt/distfeeds.conf
+opkg install python3 python3-pip --force_feeds /etc/opkg/openwrt/distfeeds.conf
+opkg install coreutils-nohup --force_feeds /etc/opkg/openwrt/distfeeds.conf
+opkg install python3-light --force_feeds /etc/opkg/openwrt/distfeeds.conf
+opkg install python3-codecs --force_feeds /etc/opkg/openwrt/distfeeds.conf
+opkg install python3-decimal --force_feeds /etc/opkg/openwrt/distfeeds.conf
+opkg install python3-logging --force_feeds /etc/opkg/openwrt/distfeeds.conf
+opkg install python3-pyserial --force_feeds /etc/opkg/openwrt/distfeeds.conf
 
 timeout=30
 
@@ -41,13 +41,32 @@ uci set gps.gpsd.beidou_sup='3'
 /etc/init.d/losant_comap enable
 /etc/init.d/losant_gps enable
 
+# add firewall rule
+uci set firewall.25=rule
+uci set firewall.25.src='wan'
+uci set firewall.25.name='Allow VPN Webaccess'
+uci set firewall.25.target='ACCEPT'
+uci set firewall.25.priority='15'
+uci add_list firewall.25.dest_port='80'
+uci add_list firewall.25.dest_port='443'
+uci set firewall.25.proto='tcp'
+uci set firewall.25.enabled='1'
+uci add_list firewall.25.src_ip='192.168.0.0/16'
+uci add_list firewall.25.src_ip='192.168.28.0/24'
+uci add_list firewall.25.src_ip='192.168.4.0/23'
+uci set firewall.25.extra='-m policy --dir in --pol ipsec --mode tunnel --tunnel-dst 0.0.0.0/0 --tunnel-src 0.0.0.0/0'
+uci commit
+
+# reload the firewall daemon
+/etc/init.d/firewall reload
+
 echo -e "\n"
 echo "Make sure to set the static lease for "comap" at ***.***.***.051"
 echo -e "\n"
 
 echo "Do you want to reboot now? (y/n): "
-# Wait for 30 seconds. If no input is received, proceed as if 'n' was entered
-read -t 30 choice
+# Wait for 300 seconds. If no input is received, proceed as if 'n' was entered
+read -t 300 choice
 
 # If read times out, $? is 1, so we can handle that scenario
 if [ $? -eq 1 ]; then
